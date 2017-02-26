19f979541c85dc346dd901440b51cefa534bdad0
gaopinghuang0 <gaopinghuang@gmail.com>
2017-02-25 22:34:28 -0500

    retry challenge7

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Practice on http://www.pythonchallenge.com/
 all code in solutions.py
 
 official solution: https://the-python-challenge-solutions.hackingnote.com/
+
+## notes
+
+### challenge 5 - about pickle
+* Python has a more primitive serialization module called marshal, but in general pickle should always be the preferred way to serialize Python objects. 
+* marshal exists primarily to support Python’s .pyc files.
+* classes are pickled by named reference, so the same restrictions in the unpickling environment apply. Note that none of the class’s code or data is pickled. **These restrictions are why picklable functions and classes must be defined in the top level of a module.**
--- solutions.py
+++ solutions.py
@@ -4,7 +4,7 @@ from __future__ import division, unicode_literals # boilerplate
 import string, StringIO
 import re
 import requests, urllib
-import Image
+from PIL import Image  # Pillow
 from bs4 import BeautifulSoup, Comment
 
 headers = { "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; "\
@@ -22,10 +22,10 @@ def get_comment(url, index):
 	comments = soup(url).findAll(text=lambda text:isinstance(text, Comment))
 	return comments[index]
 
-def cha0():
+def challenge0():
 	print "change url to 2**38", 2**38
 
-def cha1():
+def challenge1():
 	def shift2(x):
 		return chr(ord('a') + (ord(x) - ord('a') + 2) % 26) if x.isalpha() else x
 
@@ -39,7 +39,7 @@ def cha1():
 	trans("map")  # ocr
 	cha1_another(s)
 
-def cha1_another(original, shift=2):
+def challenge1_another(original, shift=2):
 	# Credit: inspired by https://the-python-challenge-solutions.hackingnote.com/level-1.html
 	a2z = string.ascii_lowercase
 	table = string.maketrans(
@@ -47,22 +47,23 @@ def cha1_another(original, shift=2):
 	)
 	print str(original).translate(table)
 
-def cha2():
+def challenge2():
 	uri = "http://www.pythonchallenge.com/pc/def/ocr.html"
 	mess_data = get_comment(uri, 1)
 	print re.sub(r'[^a-zA-Z]+', '', mess_data)  # equality
 	# or
 	print "".join(re.findall(r'[a-zA-Z]', mess_data))  # equality
 
-def cha3():
+def challenge3():
 	"""One small letter, surrounded by EXACTLY three big bodyguards
 	 on each of its sides."""
 
 	uri = "http://www.pythonchallenge.com/pc/def/equality.html"
 	mess_data = get_comment(uri, 0)
 	print "".join(re.findall(r'[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+', mess_data))
+	# >>> linkdedlist
 
-def cha4():
+def challenge4():
 	uri = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s"
 	nothing_rep = "and the next nothing is (\d+)"
 	nothing = "8022"  # initial is "12345", be asked to change later, re-run
@@ -76,20 +77,24 @@ def cha4():
 			break
 
 	print nothing
+	# >>> peak.html
 
-def cha5():
-	"""Don't understand..."""
+def challenge5():
 	import pickle
+	# Note: requests.get doesn't work, as it is unpickleable
 	handle = urllib.urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
 	data = pickle.load(handle)
 	handle.close()
 
-	for elt in data:  # "channel"
-		print "".join([e[0] * e[1] for e in elt])
+	print data
+	for line in data:
+		print "".join([ch * num for ch, num in line])
 
 	del pickle
+	# >>> "channel"
 
-def cha6_wrong():
+
+def challenge6_wrong():
 	"Result is: Collect the comments. But the method cannot get comment info"
 	nothing_rep = 'Next nothing is (\d+)'
 	nothing = "90052"
@@ -105,40 +110,56 @@ def cha6_wrong():
 			print nothing
 			break
 
-def cha6():
+def challenge6():
 	import zipfile, collections
-	out, nothing, f = [], "90052", "%s.txt"
-	nothing_rep = "Next nothing is (\d+)"
 
 	# Download the ZIP file from http://www.pythonchallenge.com/pc/def/channel.zip
-
 	file = zipfile.ZipFile('channel.zip')
+
+	# in readme.txt, start from 90052
+	out, nothing, f = [], "90052", "%s.txt"
+	nothing_ptn = "Next nothing is (\d+)"
 
 	while True:
 		try:
-			nothing = re.search(nothing_rep, file.read(f % nothing)).group(1)
+			content = file.read(f % nothing)
+			# print content
+			nothing = re.search(nothing_ptn, content).group(1)
 		except:
 			print file.read(f % nothing)
 			break
-
+		# collect comments for each archieve file
+		# https://docs.python.org/3/library/zipfile.html#zipfile.ZipInfo.comment
 		out.append(file.getinfo(f % nothing).comment)
 
 	print "".join(out)  # Not the big "hockey", but the letters "oxygen"!!
 
-def cha7():
-	"Copy the solution."
-	img = urllib.urlopen("http://www.pythonchallenge.com/pc/def/oxygen.png").read()
-	i = Image.open(StringIO.StringIO(img))  # Image.open requires a file-like object
+
+def challenge7():
+	response = requests.get("http://www.pythonchallenge.com/pc/def/oxygen.png")
+	im = Image.open(StringIO.StringIO(response.content))  # Image.open requires a file-like object
+	pix = im.load()
+	middle = im.size[1]/2
+
 	# repeat every 7 pixels, the bar is in the middle of img
-	row = [i.getpixel((x, i.size[1] / 2)) for x in range(0, i.size[0], 7)]
-	ords = [r for r, g, b, a in row if r == g == b]  # only look at rgb
+	for x in range(0, im.size[0], 7):
+		r = pix[x, middle][0]
+		# print chr(r),
+
+	hint = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+	print "".join(map(chr, hint))
+
+	###### Below is solution from Internet
+	# row = [i.getpixel((x, i.size[1] / 2)) for x in range(0, i.size[0], 7)]
+	# ords = [r for r, g, b, a in row if r == g == b]  # only look at rgb
 	# print "".join(map(chr, ords))
 	# print "".join(map(chr, [105, 110, 116, 101, 103, 114, 105, 116, 121]))
 	# or merge above two lines into:
-	print "".join(map(chr, map(int, re.findall("\d+", "".join(map(chr, ords))))))
+	# print "".join(map(chr, map(int, re.findall("\d+", "".join(map(chr, ords))))))
+
 
 def main():
-	cha1()
+	challenge7()
 
 if __name__ == '__main__':
 	main()
