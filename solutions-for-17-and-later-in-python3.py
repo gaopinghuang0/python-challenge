7c3d51a60f95b646e0e108fc03e3528e1097b88a
gaopinghuang0 <gaopinghuang@gmail.com>
2017-07-07 22:48:17 -0400

    challenge 22 -- completely don't understand

--- solutions-for-17-and-later-in-python3.py
+++ solutions-for-17-and-later-in-python3.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 import requests, re
+from PIL import Image, ImageDraw  # pillow
 
 
 def challenge17():
@@ -190,10 +191,36 @@ def challenge21():
         # http://www.pythonchallenge.com/pc/hex/copper.html
 
 
+# completely don't understand
+def challenge22():
+    img = Image.open('22/white.gif')
+    print(img.size)
+    new = Image.new("RGB", (500, 200))
+    draw = ImageDraw.Draw(new)
+    cx, cy = 0, 100
+    for frame in range(img.n_frames):
+        img.seek(frame)
+        left, upper, right, lower = img.getbbox()
+
+        # get the direction; like a joystick, 
+        dx = left - 100
+        dy = upper - 100
+
+        # end of a move(letter), shift to the right
+        if dx == dy == 0:
+            cx += 50
+            cy = 100
+        cx += dx
+        cy += dy
+        draw.point([cx, cy])
+
+    new.show()
+    # bonus
+    # http://www.pythonchallenge.com/pc/hex/bonus.html
 
 
 def main():
-    challenge21()
+    challenge22()
 
 if __name__ == '__main__':
     main()
